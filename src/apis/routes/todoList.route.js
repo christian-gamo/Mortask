28deab68d74c09791188785ab23eb8e990805f7a
christian-gamo <christian.gamo.pro@gmail.com>
2023-12-06 18:52:23 +0100

    db_more

--- src/apis/routes/todoList.route.js
+++ src/apis/routes/todoList.route.js
@@ -1,8 +1,8 @@
 import express from "express";
 import TodoList from "../models/todoList.model.js";
 import User from "../models/user.model.js";
-import Group from "../models/group.model.js";
-import GroupMember from "../models/todoList_members.model.js";
+import TodoListMembers from "../models/todoList_members.model.js";
+
 
 const routerTodoList = express.Router();
 
@@ -13,6 +13,31 @@ const getAllTodoLists = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const createTodoList = async (req, res) => {
+  const todoList_name = req.body.todoList_name;
+  const todoList_description = req.body.todoList_description;
+  const todoList_creator = req.body.todoList_creator;
+  const todoList_isShared = false;
+  console.log("AAAAAAAAAAAAA")
+  try {
+    const todoList = await TodoList.create({ 
+      todoList_name, 
+      todoList_description,
+      todoList_creator,
+      todoList_isShared
+    });
+    const creator = await User.findByPk(todoList_creator);
+
+    await creator.addGroup(todoList);
+
+    return res.status(201).json(todoList);
+  } 
+  catch (error) {
+    console.error('Error while creating todoList:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
@@ -28,6 +53,7 @@ const getAllPrivateTodosByID = async (req, res) => {
 };
 
 routerTodoList.get("/", getAllTodoLists);
+routerTodoList.post("/create", createTodoList);
 routerTodoList.get("/privateTodosForUser", getAllPrivateTodosByID);
 
 export default routerTodoList;
