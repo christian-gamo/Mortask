d1120f5a3df9e24aa90d05053c0c174c4b6a13a6
Taha <tahafakhran@gmail.com>
2023-12-06 18:30:32 +0100

    user trigger modification

--- src/apis/connection/usertrigger.sql
+++ src/apis/connection/usertrigger.sql
@@ -4,12 +4,16 @@ CREATE TRIGGER after_user_insert
 AFTER INSERT ON users
 FOR EACH ROW
 BEGIN
-    -- Get the last inserted group_id
-    SELECT LAST_INSERT_ID() INTO @last_group_id;
-
     -- Insert a row into the 'todoList'
-    INSERT INTO `todoList` (`todoList_name`, `todoList_description`, `todoList_creator`)
-    VALUES (CONCAT(NEW.user_fname, ' List'), 'Your Private List!', NEW.user_id);
+    INSERT INTO `todoList` (`todoList_name`, `todoList_description`, `todoList_creator`, `todoList_isShared`)
+    VALUES (CONCAT(NEW.user_fname, ' List'), 'Your Private List!', NEW.user_id, false);
+
+    -- Get the last inserted todoList_id
+    SELECT LAST_INSERT_ID() INTO @last_todoList_id;
+
+    -- Insert a row into 'todoList_members'
+    INSERT INTO `todoList_members` (`todoList_id`, `user_id`)
+    VALUES (@last_todoList_id, NEW.user_id);
 END //
 
 DELIMITER ;
