a27187b405ba45cd345155b1789ae5b4bb8f55b0
darlenushi <darlene.s15402@gmail.com>
2023-12-08 19:10:58 +0100

    front adjustments

--- src/pages/components/ViewTask.js
+++ src/pages/components/ViewTask.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 
-//push issues 
+//push issue
 function ViewTask() {
     const [openModal, setOpenModal] = useState(false);
     return (
