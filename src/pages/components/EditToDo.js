e67a9e10b32dec206a89f0701234880471fa3ab6
darlenushi <darlene.s15402@gmail.com>
2023-12-10 01:20:13 +0100

    changes

--- src/pages/components/EditToDo.js
+++ src/pages/components/EditToDo.js
@@ -33,14 +33,6 @@ function EditToDo() {
                                 <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
                                 <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Ex : Front-end project" required />
                             </div>
-                            <div>
-                                <label htmlFor="unsharetodo" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Remove user from to-do</label>
-                                <select id="unsharetodo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                                    <option value="none">None</option>
-                                    <option>Popi</option>
-                                    <option>Pipo</option>
-                                </select>
-                            </div>
                             <div className="sm:col-span-2">
                                 <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
                                 <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Ex : All the tasks related to FE project"></textarea>
